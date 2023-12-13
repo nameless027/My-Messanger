d4dc959dd5648c4c0b03ae9eacdddbf73c4cfe0e
Vlad <nameless027@gmai.com>
2023-12-13 16:36:15 +0300

    Создание сообщения

--- ConsoleClient/My-Messanger/Program.cs
+++ ConsoleClient/My-Messanger/Program.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using Newtonsoft.Json;
+
+
+
+
+
 namespace My_Messanger
 {
     class Program
     {
         static void Main()
         {
-            Message msg = new Message();
+            Message msg = new Message("Nameless027", "Privet", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg); 
+            Console.WriteLine(output);
+            
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
 
-            Console.WriteLine("Начало проекта");
-            Console.WriteLine(msg.ToString());
+
+            //{ "UserName":"Nameless027","MessageText":"Privet","TimeStamp":"2023-12-13T13:33:56.503903Z"}
+            //Nameless027 < 13.12.2023 13:33:56 >: Privet
+
         }
     }
 }
