d678d9dfbd8e2cd1485241ff6e3d79120a205696
Vlad_G <nameless027@gmai.com>
2023-12-14 11:51:40 +0300

    коммит

--- ConsoleClient/My-Messanger/Program.cs
+++ ConsoleClient/My-Messanger/Program.cs
@@ -19,8 +19,7 @@ namespace My_Messanger
             Console.WriteLine(deserializedMsg);
 
 
-            //{ "UserName":"Nameless027","MessageText":"Privet","TimeStamp":"2023-12-13T13:33:56.503903Z"}
-            //Nameless027 < 13.12.2023 13:33:56 >: Privet
+           
 
         }
     }
