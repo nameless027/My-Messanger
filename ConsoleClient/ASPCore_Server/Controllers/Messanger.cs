0894d63edf4be547e4645dd598dd492d15a27bed
Vlad_G <nameless027@gmai.com>
2023-12-14 12:32:38 +0300

    Пдключение ASPCore

---
+++ ConsoleClient/ASPCore_Server/Controllers/Messanger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPCore_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Messanger : ControllerBase
+    {
+       
+
+        // GET api/<Messanger>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Раз, два , три" + id.ToString();
+        }
+
+        // POST api/<Messanger>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        
+        
+    }
+}
