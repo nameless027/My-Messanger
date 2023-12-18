2d59a16422ba0745b9b59a7a1e0f9cdb76e5f3d0
Vlad_G <nameless027@gmai.com>
2023-12-18 16:34:36 +0300

    Подключение терминала GUI

---
+++ CosoleMessangerWihtGUI/Message.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+
+// Хранение сообщений
+namespace My_Messanger
+{
+    [Serializable] // Подключение сриализации для преобразовании объекта в поток байтов для передачи. Необходим публичый класс
+    public class Message
+
+    { 
+        // Три поля Сообщения (имя, текс, время отправки):
+        public string UserName { get; set; }
+        public string MessageText { get; set; }
+        public DateTime TimeStamp { get; set; }
+     
+
+
+        // Конструктор
+        public Message(DateTime timeStamp, string userName, string messageText)
+        {
+            UserName = userName;
+            MessageText = messageText;
+            TimeStamp = timeStamp;
+        }
+
+
+        //Конструктор
+        public Message(string v, string v1)
+        {
+            UserName = "System";
+            MessageText = "Servis is running...";
+            TimeStamp = DateTime.Now;
+        }
+
+        // Переопределение выходного текста
+        public override string ToString()
+        {
+            string output = String.Format("{0} <{2}>: {1}", UserName, MessageText, TimeStamp);
+            return output;
+        }
+    }
+}
---
+++ CosoleMessangerWihtGUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using My_Messanger;
+using Terminal.Gui;
+
+
+namespace ConsoleMessangerWihtGUI
+{
+    internal class Program
+    {
+
+
+        private static List<Message> messages = new List<Message>();
+        private static MessangerClientAPI API = new MessangerClientAPI();
+
+        private static MenuBar menu;
+        private static Window winMain;
+        private static Window WinMessages;
+        private static Label labelUser;
+        private static TextField fieldUserName;
+        private static Label LebelMessage;
+        private static TextField fieldMessage;
+        private static Button btnSend;
+
+        static void Main()
+        {
+
+        }
+    }
+}
